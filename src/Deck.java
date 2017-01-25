b3d87e9fdea6c676743ba62174bc58a0b37cd6a4
Jordy van Londen <jordy.vanlonden@gmail.com>
2017-01-25 13:27:22 +0100

    versie Jordy

--- src/Deck.java
+++ src/Deck.java
@@ -1,31 +1,48 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class Deck {
-	private HashMap<String, Integer> deck;
-	
+	private Map<Character, Integer> deck;
+
+
 	public Deck() {
-		 //Initiate combo's in HashMap
-		this.deck = new HashMap<String, Integer>();
-		this.deck.put("A", 4);
-		this.deck.put("K", 4);
-		this.deck.put("Q", 4);
-		this.deck.put("J", 4);
-		this.deck.put("T", 4);
-		this.deck.put("9", 4);
-		this.deck.put("8", 4);
-		this.deck.put("7", 4);
-		this.deck.put("6", 4);
-		this.deck.put("5", 4);
-		this.deck.put("4", 4);
-		this.deck.put("3", 4);
-		this.deck.put("2", 4);
+        this.deck = new HashMap<>();
+		this.deck.put('A', 4);
+		this.deck.put('K', 4);
+		this.deck.put('Q', 4);
+		this.deck.put('J', 4);
+		this.deck.put('T', 4);
+		this.deck.put('9', 4);
+		this.deck.put('8', 4);
+		this.deck.put('7', 4);
+		this.deck.put('6', 4);
+		this.deck.put('5', 4);
+		this.deck.put('4', 4);
+		this.deck.put('3', 4);
+		this.deck.put('2', 4);
 	}
-	
-	public void removeCard(String card) {
+
+    public int addCard(char card)
+    {
+        int numberOfCards = this.deck.get(card) + 1;
+        this.deck.put(card, numberOfCards);
+
+        return numberOfCards;
+    }
+
+	public int removeCard(char card) {
 		if ( ! this.deck.containsKey(card)) {
-			System.err.println("Your hand consist of a card which cannot be a legitimate card");
+			System.err.println("Your hand consist of a card which cannot be a legitimate card.");
 		}
-		
-		this.deck.put(card, this.deck.get(card) - 1);
+
+        int numberOfCards = this.deck.get(card) - 1;
+        this.deck.put(card, numberOfCards);
+
+        return numberOfCards;
 	}
+
+    public int getNumber(char card)
+    {
+        return this.deck.get(card);
+    }
 }
